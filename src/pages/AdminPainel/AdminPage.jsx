308ca525fa7541117d979eddb25dd88c81268c1d
Carlos Alexandre <alexfilho062005@gmail.com>
2025-03-15 10:47:31 -0300

    :lipstick: feat: styles card user admin

--- src/pages/AdminPainel/AdminPage.jsx
+++ src/pages/AdminPainel/AdminPage.jsx
@@ -1,9 +1,10 @@
 import { InputOtpAdmin } from '../AdminPainel/components/InputOTP';
 import { AuthContext } from '@/contexts/AuthContext';
-import { User } from 'lucide-react';
+import { Check, RefreshCcw } from 'lucide-react';
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../api';
+import { CardUser } from './components/CardUser';
 
 export function AdminPage() {
   const { userData } = useContext(AuthContext);
@@ -32,17 +33,17 @@ export function AdminPage() {
     <div className="wrapper py-16  flex items-center justify-center flex-col gap-8">
       <h1 className=" text-4xl font-bold">ADMIN PAINEL</h1>
       <InputOtpAdmin />
-      <div className="flex gap-8 self-start">
+      <div className="flex flex-wrap gap-8 self-start">
         {users.map((user) => (
-          <div className="flex flex-col items-center justify-center p-6 w-[300px] self-start  rounded-lg">
-            <div className="bg-[#D9D9D9] w-full rounded-t-lg flex justify-center p-6">
-              <User size={100} />
-            </div>
-            <div className="bg-[#3C3C3C] w-full  justify-center p-6 rounded-b-lg flex flex-col items-center gap-4">
-              <h1 className="text-2xl font-bold">{user?.username}</h1>
-              <h2>atualizar usuario</h2>
-            </div>
-          </div>
+          <CardUser
+            key={user._id}
+            profilePhotoUrl={user.profilePhotoUrl}
+            username={user.username}
+            matricula={user.matricula}
+            typeOfPlan={user.typeOfPlan}
+            startDate={user.startDate}
+            endDate={user.endDate}
+          />
         ))}
       </div>
     </div>
---
+++ src/pages/AdminPainel/components/CardUser.jsx
@@ -0,0 +1,55 @@
+import { Check, RefreshCcw, X } from 'lucide-react';
+
+export function CardUser({
+  profilePhotoUrl,
+  username,
+  matricula,
+  typeOfPlan,
+  startDate,
+  endDate,
+}) {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center p-6 w-[450px] self-start  rounded-lg">
+      <div className="bg-[#D9D9D9] w-full rounded-t-lg flex justify-center p-2">
+        <img className="rounded-full size-28" src={profilePhotoUrl} alt="" />
+      </div>
+      <div className="bg-[#3C3C3C] w-full justify-center p-6 rounded-b-lg flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold">{username}</h1>
+        <h2>Matricula: {matricula}</h2>
+        <div className="flex justify-center w-full gap-2 items-center p-2 rounded-lg bg-[#4A4A4A]">
+          <Check className="bg-green-800 text-green-300 rounded-full" />
+          <span>Pago</span>
+        </div>
+        <div className="flex justify-center w-full gap-2 items-center p-2 rounded-lg bg-[#4A4A4A] cursor-pointer hover:opacity-80 duration-300">
+          <RefreshCcw className="bg-green-800 rounded-full p-1" />
+          <span>Atualizar usuário</span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span>Tipo plano:</span>
+          <span className="first-letter:uppercase font-bold">{typeOfPlan}</span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span>Iniciado:</span>
+          <span>{formatDate(startDate)}</span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span>Finaliza:</span>
+          <span>{formatDate(endDate)}</span>
+        </div>
+        <div className="flex justify-center w-full gap-2 items-center p-2 rounded-lg bg-[#4A4A4A] cursor-pointer hover:opacity-80 duration-300">
+          <X className="bg-red-800 rounded-full p-1" />
+          <span>Apagar usuário</span>
+        </div>
+      </div>
+    </div>
+  );
+}
